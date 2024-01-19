4341a5c76c5023e19c9702b2a8b3dad183f89e1a
Pablo Gil de San Nicolás <56157762+alu0100977554@users.noreply.github.com>
2024-01-19 11:53:27 +0000

    CollectObservations() from MoveToGoalAgent overrides parent's
    It now collects agent's and target's positions as observations

--- Assets/MoveToGoalAgent.cs
+++ Assets/MoveToGoalAgent.cs
@@ -7,13 +7,15 @@ using Unity.MLAgents.Sensors;
 
 public class MoveToGoalAgent : Agent
 {
+    [SerializeField] private Transform _targetTransform;
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);          // Adding Agent's position to Observation vector
+        sensor.AddObservation(_targetTransform.position);   // Adding Target's position to Observation vector
+    }
+
     public override void OnActionReceived(ActionBuffers actions)
     {
         base.OnActionReceived(actions);
-    }
-
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        base.CollectObservations(sensor);
     }
 }
